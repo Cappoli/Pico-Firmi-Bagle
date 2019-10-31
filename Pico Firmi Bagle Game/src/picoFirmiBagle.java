ba57bc6a940c966d556cb214237dbdd4fe5b35db
cappoli <Cappoli567@gmail.com>
2019-10-31 17:29:53 -0400

    adding

--- Pico Firmi Bagle Game/src/picoFirmiBagle.java
+++ Pico Firmi Bagle Game/src/picoFirmiBagle.java
@@ -3,19 +3,39 @@ public class picoFirmiBagle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a;
-		int b;
-		int c;
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		
+		int guessA = 0;
+		int guessB = 0;
+		int guessC = 0;
+		int guessCheck = 0;
+		int correctAnswer = 0;
+		
+		
+		
+		String pico = "Pico";
+		String firmi = "Firmi";
+		String bagel = "Bagel";
+		
 		
 		//Generate three random single digit numbers 0-0-0 to 9-9-9
 		//Player enters 3 numbers
 		//Check if player has guessed any number in the correct spot
 		//Check to see if player has guessed the correct number in any slot
+		
 		/* Output results
 		 * 
 		 * 			Pico - used number in wrong spot
 		 * 			Firmi - used number in correct spot
-		 * 			Bagle - no numbers guessed are used */
+		 * 			Bagel - no numbers guessed are used */
+		
+		
+		if( guessA == a ) {//Firmi is returned} 
+		if( guessA == b || guessA == c) {//Pico is returned} 
+		if( guessA != a && guessA != b && guessA != c ) {//Bagel is returned} 
+		
 		//Remember guesses and results
 		//
 
