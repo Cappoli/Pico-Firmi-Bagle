c0ee13da6684a8fdb763e3ee12a3ccb644b5cbaa
cappoli <Cappoli567@gmail.com>
2019-10-30 21:53:17 -0400

    PFB

---
+++ Pico Firmi Bagle Game/src/picoFirmiBagle.java
@@ -0,0 +1,24 @@
+
+public class picoFirmiBagle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int c;
+		
+		//Generate three random single digit numbers 0-0-0 to 9-9-9
+		//Player enters 3 numbers
+		//Check if player has guessed any number in the correct spot
+		//Check to see if player has guessed the correct number in any slot
+		/* Output results
+		 * 
+		 * 			Pico - used number in wrong spot
+		 * 			Firmi - used number in correct spot
+		 * 			Bagle - no numbers guessed are used */
+		//Remember guesses and results
+		//
+
+	}
+
+}
