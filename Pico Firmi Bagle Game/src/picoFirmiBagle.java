8bb04536bc3a80c7930e27828e7451f043280d07
cappoli <Cappoli567@gmail.com>
2019-11-03 17:05:03 -0500

    Restructure 1

--- Pico Firmi Bagle Game/src/picoFirmiBagle.java
+++ Pico Firmi Bagle Game/src/picoFirmiBagle.java
@@ -1,32 +1,34 @@
 
 public class picoFirmiBagle {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	
+	int guessA = 0;
+	int guessB = 0;
+	int guessC = 0;
+	int guessCheck = 0;
+	int correctAnswer = 0;
+	
+	String outcomeA;
+	String outcomeB;
+	String outcomeC;
+			
+	String pico = "Pico";
+	String firmi = "Firmi";
+	String bagel = "Bagel";
 
 	public static void main(String[] args) {
+	}
 		// TODO Auto-generated method stub
-		int a = 0;
-		int b = 0;
-		int c = 0;
 		
-		int guessA = 0;
-		int guessB = 0;
-		int guessC = 0;
-		int guessCheck = 0;
-		int correctAnswer = 0;
-		
-		String outcomeA;
-		String outcomeB;
-		String outcomeC;
-		
-		
-		
-		String pico = "Pico";
-		String firmi = "Firmi";
-		String bagel = "Bagel";
-		
-		
+				
 		//Generate three random single digit numbers 0-0-0 to 9-9-9
+		
 		//Player enters 3 numbers
+		
 		//Check if player has guessed any number in the correct spot
+		
 		//Check to see if player has guessed the correct number in any slot
 		
 		/* Output results
@@ -35,12 +37,12 @@ public class picoFirmiBagle {
 		 * 			Firmi - used number in correct spot
 		 * 			Bagel - no numbers guessed are used */
 		
-		
+		public void comparator() {
 		if( guessA == a ) {			//Firmi is returned
 			outcomeA = firmi;
 		}else if( guessA == b || guessA == c) {		//Pico is returned
 			outcomeA = pico;
-		}else if( guessA != a && guessA != b && guessA != c ) {//Bagel is returned}
+		}else if( guessA != a && guessA != b && guessA != c ) {//Bagel is returned
 			outcomeA = bagel;
 		}
 		
@@ -48,7 +50,7 @@ public class picoFirmiBagle {
 			outcomeB = firmi;
 		}else if( guessB == a || guessB == c) {		//Pico is returned
 			outcomeB = pico;
-		}else if( guessB != b && guessB != a && guessB != c ) {//Bagel is returned}
+		}else if( guessB != b && guessB != a && guessB != c ) {//Bagel is returned
 			outcomeB = bagel;
 		}
 		
@@ -56,13 +58,13 @@ public class picoFirmiBagle {
 			outcomeC = firmi;
 		}else if( guessC == b || guessC == a) {		//Pico is returned
 			outcomeC = pico;
-		}else if( guessC != a && guessC != b && guessC != c ) {//Bagel is returned}
+		}else if( guessC != a && guessC != b && guessC != c ) {//Bagel is returned
 			outcomeC = bagel;
+		}
+		
 		}
 		
 		//Remember guesses and results
 		//
-
-	}
 
 }
